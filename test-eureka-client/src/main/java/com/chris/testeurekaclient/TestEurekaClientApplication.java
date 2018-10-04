40d24967d87e2c3b26e5db483583aa33f4a9c81e
Chris He <nnchris@qq.com>
2018-10-04 15:59:55 +0800

    Add eureka-server / eureka-client / ribbon / Hystrix project

---
+++ test-eureka-client/src/main/java/com/chris/testeurekaclient/TestEurekaClientApplication.java
@@ -0,0 +1,15 @@
+package com.chris.testeurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class TestEurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestEurekaClientApplication.class, args);
+    }
+}
---
+++ test-ribbon-consumer/src/main/java/com/chris/ribbon/controller/ConsumerController.java
@@ -0,0 +1,20 @@
+package com.chris.ribbon.controller;
+
+import com.chris.ribbon.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class ConsumerController {
+
+    @Autowired
+    private HelloService helloService;
+
+    @RequestMapping(value = "/ribbon-consumer", method = RequestMethod.GET)
+    public String helloConsumer() {
+        return helloService.callHelloService();
+    }
+}
